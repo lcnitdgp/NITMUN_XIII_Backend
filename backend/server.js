34fcccaeb14e1da7fbf533e1ac61fe4f2671b470
Manoj <stdiomozz313@gmail.com>
2025-01-19 11:02:58 +0530

    rate limit

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const rateLimit = require('express-rate-limit');
+
 const mongoose = require('mongoose')
 const port = process.env.PORT || 5000;
 //const dotenv = require("dotenv");
@@ -13,12 +15,22 @@ const registrationsRoute = require("./routes/registrations");
 const mailer = require("./routes/nodemailer");
 //const port = https://nitmun-xiii-backend.onrender.com
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 30, // Limit each IP to 100 requests per `window` (15 minutes)
+    standardHeaders: 'draft-8', // Use the combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// Apply the rate limiter to all requests
+
+
 var cors = require('cors')
 app.use(cookieParser())
-
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cookieParser());
+app.use(limiter);
 //dotenv.config();
 //const {MONGOURI}= require('./keys.js')
 //const {MONGOURI} = process.env.MONGOURI
